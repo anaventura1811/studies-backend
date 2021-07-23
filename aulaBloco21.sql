cd0b418eca59dd89c7415155773cdee6da33a8e7
Ana Ventura <anaventura1811@gmail.com>
2021-07-23 16:15:20 -0300

    Exercícios de fixação sobre datas: Exercício 2

--- aulaBloco21.sql
+++ aulaBloco21.sql
@@ -255,5 +255,8 @@ SELECT YEAR(CURRENT_DATE()); -- retorna o ano atual
 SELECT HOUR(NOW()); -- retorna a hora atual
 
 -- Exercícios de Fixação sobre Datas:
--- 1 - Monte uma query que exiba a diferença de dias entre '2030-01-20' e hoje.
+-- 1 - Monte uma query que exiba a diferença de dias entre '2030-01-20' e hoje
 SELECT DATEDIFF('2030-01-20', '2021-07-23');
+
+-- 2 - Monte uma query exiba a diferença de horas entre '10:25:45' e '11:00:00' 
+SELECT TIMEDIFF('10:25:45', '11:00:00');
