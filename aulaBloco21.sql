7840aad6cd211e56f9447af5b2faf28c59196cf8
Ana Ventura <anaventura1811@gmail.com>
2021-07-23 15:19:10 -0300

    Exercício 6 - Condicionais

--- aulaBloco21.sql
+++ aulaBloco21.sql
@@ -115,3 +115,10 @@ SELECT
     END AS valor
 FROM sakila.customer
 LIMIT 10;
+
+-- Exercício 6 
+
+SELECT film_id, title,
+IF (title = 'ACE GOLDFINGER', 'Já assisti a esse filme', 'Não conheço o filme')
+AS 'conheço_o_filme?'
+FROM sakila.film;
