7bbcebade7619cbdcd28dc87f725c4e0b933bc8c
Ana Ventura <anaventura1811@gmail.com>
2021-07-23 16:09:50 -0300

    Funções DATEDIFF, TIMEDIFF

--- aulaBloco21.sql
+++ aulaBloco21.sql
@@ -231,4 +231,13 @@ SELECT CEIL(85.234);
 SELECT CURRENT_DATE(); -- YYYY-MM-DD
 SELECT NOW(); -- YYYY-MM-DD HH:MM:SS
 
+-- Diferença em dias entre duas datas ou entre dois horários:
+-- 30, ou seja, a primeira data é 30 dias depois da segunda
+SELECT DATEDIFF('2020-01-31', '2020-01-01');
+
+-- -30, ou seja, a primeira data é 30 dias antes da segunda
+SELECT DATEDIFF('2020-01-01', '2020-01-31');
+
+-- -01:00:00, ou seja, há 1 hora de diferença entre os horários
+SELECT TIMEDIFF('08:30:10', '09:30:10');
 
