081db2756353bacad8315c254d7bbb8602610546
Ana Ventura <anaventura1811@gmail.com>
2021-07-23 15:55:59 -0300

    Arrendondamento de valores com SQL

--- aulaBloco21.sql
+++ aulaBloco21.sql
@@ -176,3 +176,11 @@ SELECT CASE
     WHEN (220 MOD 12) > 0 AND (220 MOD 12 > 12 AND 220 MOD 12 <= 24)  THEN (220 DIV 12) + 2
     ELSE 'Podemos levar' + (220 DIV 12)
     END AS 'podemos levar o seguinte número de grupos completos';
+
+-- Arrendondamento de valores com SQL:
+-- Podemos omitir ou especificar quantas casas decimais queremos
+SELECT ROUND(10.4925); -- 10
+SELECT ROUND(10.5136); -- 11
+SELECT ROUND(-10.5136); -- -11
+SELECT ROUND(10.4925, 2); -- 10.49
+SELECT ROUND(10.4925, 3); -- 10.493
