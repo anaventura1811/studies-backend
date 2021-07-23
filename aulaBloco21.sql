a8518058a284036770cf22b6c8b38e55e78cd382
Ana Ventura <anaventura1811@gmail.com>
2021-07-23 16:04:46 -0300

    Exercício de fixação 2

--- aulaBloco21.sql
+++ aulaBloco21.sql
@@ -215,3 +215,6 @@ SELECT ROUND(7 + (RAND() * 6));
 
 -- Monte uma query que gere um valor entre 15 e 20:
 SELECT ROUND(15 + (RAND() * 5));
+
+-- Monte uma query que exiba o valor arredondado de 15.7515971 com uma precisão de 5 casas decimais:
+SELECT ROUND(15.7515971, 5);
