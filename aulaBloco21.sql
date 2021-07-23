7e9f2d099a27e9a39c94491259ad144ed01c0e54
Ana Ventura <anaventura1811@gmail.com>
2021-07-23 16:06:51 -0300

    Exercício de fixação 4

--- aulaBloco21.sql
+++ aulaBloco21.sql
@@ -223,5 +223,9 @@ SELECT ROUND(15.7515971, 5);
 -- Qual é o valor aproximado para baixo dessa média?
 SELECT FLOOR(39.494);
 
+-- Temos uma taxa de inscrição de 85.234% no curso de fotografia para iniciantes.
+-- Qual é o valor aproximado para cima dessa média?
+SELECT CEIL(85.234);
+
 
 
