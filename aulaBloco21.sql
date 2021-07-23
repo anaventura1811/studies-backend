0b9f5b5034f1c4dc29eab84a6095480f1f540049
Ana Ventura <anaventura1811@gmail.com>
2021-07-23 15:24:51 -0300

    Exercício 7

--- aulaBloco21.sql
+++ aulaBloco21.sql
@@ -108,9 +108,12 @@ SELECT
     first_name,
     email,
     CASE
-        WHEN email = 'MARY.SMITH@sakilacustomer.org' THEN 'Cliente de baixo valor'
-        WHEN email = 'PATRICIA.JOHNSON@sakilacustomer.org' THEN 'Cliente de médio valor'
-        WHEN email = 'LINDA.WILLIAMS@sakilacustomer.org' THEN 'Cliente de alto valor'
+        WHEN 
+          email = 'MARY.SMITH@sakilacustomer.org' THEN 'Cliente de baixo valor'
+        WHEN
+          email = 'PATRICIA.JOHNSON@sakilacustomer.org' THEN 'Cliente de médio valor'
+        WHEN 
+          email = 'LINDA.WILLIAMS@sakilacustomer.org' THEN 'Cliente de alto valor'
         ELSE 'não classificado'
     END AS valor
 FROM sakila.customer
@@ -118,7 +121,22 @@ LIMIT 10;
 
 -- Exercício 6 
 
-SELECT film_id, title,
-IF (title = 'ACE GOLDFINGER', 'Já assisti a esse filme', 'Não conheço o filme')
-AS 'conheço_o_filme?'
+SELECT
+  film_id, title,
+IF 
+  (title = 'ACE GOLDFINGER', 'Já assisti a esse filme', 'Não conheço o filme')
+AS 
+  'conheço_o_filme?'
+FROM 
+  sakila.film;
+
+-- Exercício 7
+SELECT title, rating,
+	CASE
+		WHEN rating = 'G' THEN 'Livre para todos'
+        WHEN rating = 'PG' THEN 'Não recomendado para menores de 10 anos'
+        WHEN rating = 'PG-13' THEN 'Não recomendado para menores de 13 anos'
+        WHEN rating = 'R' THEN 'Não recomendado para menores de 17 anos'
+	ELSE 'Proibido para menores de idade'
+END AS 'público_alvo'
 FROM sakila.film;
