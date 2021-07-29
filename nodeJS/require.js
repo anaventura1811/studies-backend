bf251336fd4af31d21b7d1e6a21b8d57c6a4eb86
Ana Ventura <anaventura1811@gmail.com>
2021-07-28 21:03:49 -0300

    objeto module - Node

--- nodeJS/require.js
+++ nodeJS/require.js
@@ -1,4 +1,8 @@
-const path = require('path');
+// const path = require('path');
 // Maneira de chamar módulos nativos e/ou criados, instalados no node
 // path é módulo nativo do node
-console.log(path)
+// console.log(path)
+// console.log(path.basename(__filename))
+// meus módulos:
+const meuModulo = require('./exports.js');
+console.log(meuModulo)
