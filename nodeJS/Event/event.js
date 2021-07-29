994f5a5ff603e372e19d5fbb6332bfb9b9f59e85
Ana Ventura <anaventura1811@gmail.com>
2021-07-29 00:49:20 -0300

    entendendo a diferença entre event listeners no node

--- nodeJS/Event/event.js
+++ nodeJS/Event/event.js
@@ -6,7 +6,10 @@ console.log(ev);
 
 ev.on('saySomething', () => {
   console.log('Ouvindo o evento')
-}); // event listener
+}); // event listener - ouvir sempre 
+
+ev.once('saySomething', () => console.log('Ouvindo uma vez só')); // ouvir uma única vez
+
 
 ev.emit('saySomething'); // emitir eventos com emit
 ev.emit('saySomething');
