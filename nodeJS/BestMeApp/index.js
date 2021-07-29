03f0cf47169cd1a843ba0765821e955f8b35d933
Ana Ventura <anaventura1811@gmail.com>
2021-07-28 23:45:20 -0300

    Entendendo stdout - Node

---
+++ nodeJS/BestMeApp/index.js
@@ -0,0 +1,12 @@
+const questions = [
+  "O que aprendi hoje?",
+  "O que me deixou aborrecido(a)? E o que eu poderia fazer para me sentir melhor?",
+  "O que me deixou feliz hoje?",
+  "Quantas pessoas ajudei hoje?",
+];
+
+const ask = (index = 0) => {
+  process.stdout.write(questions[index] + "\n\n\n")
+}
+
+ask();
