b209da471e01bfaa8825529eb251ab1aa6560634
Ana Ventura <anaventura1811@gmail.com>
2021-07-29 10:48:48 -0300

    configurações servidor e ejs

--- nodeJS/NodeEJS/server.js
+++ nodeJS/NodeEJS/server.js
@@ -1,3 +1,14 @@
 const express = require('express');
 const app = express();
 app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+  res.render("index");
+});
+
+app.get("/about", function(req, res) {
+  res.render("about");
+});
+
+app.listen(8080);
+console.log("Rodando");
