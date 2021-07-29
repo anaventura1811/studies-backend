c5fc2a5331786aa1c8c30655e523a5f1d514ab72
Ana Ventura <anaventura1811@gmail.com>
2021-07-28 21:09:08 -0300

    mais sobre process arqv

--- nodeJS/process.js
+++ nodeJS/process.js
@@ -1,4 +1,7 @@
 console.log(process.argv)
 // lista de argumentos sendo executadas no node
 
-console.log('meu nome é ', process.argv[2] + ' ' + process.argv[3])
+// console.log('meu nome é ', process.argv[2] + ' ' + process.argv[3])
+const first_name = process.argv[2];
+const last_name = process.argv[3];
+console.log(`Seu nome é ${first_name} ${last_name}`)
